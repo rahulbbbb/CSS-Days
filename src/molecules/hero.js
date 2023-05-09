08a1f9e83cb1c516710f215eaab68e352a5f7d5a
Rahulbbbb <beingbankoti@gmail.com>
2023-05-09 20:42:12 +0530

    first commit

---
+++ src/molecules/hero.js
@@ -0,0 +1,17 @@
+import styles from './hero.module.css'
+function Hero(){
+  return(
+    <div className={styles.container}>
+        <img className={styles.image} src ="https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/03/Konohagakure.jpg" />
+    <div className={styles.content}>
+        <h1>Konoha Village</h1>
+        <p className={styles.text}>The Hidden Leaf Village is the first village that was ever formed by Shinobi.
+                                   <br/><br/> The Senju and Uchiha clan had been rivals for years before they came to an agreement with the help of Hashirama Senju and Madara Uchiha. 
+                                   <br/><br/> The two of them built a village together and called it the Hidden Leaf Village (Konoha).</p>
+                                   
+        <button className={styles.button}>Buy Now</button>
+    </div>
+    </div>
+  )
+}
+export default Hero;
---
+++ src/test.js
@@ -0,0 +1,10 @@
+Konoha Village
+
+
+The Hidden Leaf Village is the first village that was ever formed by Shinobi. <br/> The Senju and Uchiha clan had been rivals for years before they came to an agreement with the help of Hashirama Senju and Madara Uchiha. <br/> The two of them built a village together and called it the Hidden Leaf Village (Konoha).
+
+Buy Now
+
+
+
+https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/03/Konohagakure.jpg
