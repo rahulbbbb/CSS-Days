addaa65767220c6778379ed0dcfca00095f21c92
Rahulbbbb <beingbankoti@gmail.com>
2023-05-13 02:08:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PendingTasksList from "./component/Molecules/PendingTasksList";
+import './App.css'
 
 function App() {
+  const tasks = [
+    { id: 1, title: "Task 1", completed: false },
+    { id: 2, title: "Task 2", completed: false },
+    { id: 3, title: "Task 3", completed: false },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <PendingTasksList tasks={tasks} />
     </div>
   );
 }
 
 export default App;
+
